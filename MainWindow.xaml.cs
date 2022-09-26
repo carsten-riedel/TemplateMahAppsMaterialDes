38a6e770cf555e25cdd27a438cd2da183bf091a5
Carsten Riedel <carstenriedel@outlook.com>
2022-09-26 07:35:59 +0200

    template up

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MahAppsMaterialHamburger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+            // Launch the GitHub site...
+        }
+
+        private void DeployCupCakes(object sender, RoutedEventArgs e)
+        {
+            Keyboard.ClearFocus();
+            // deploy some CupCakes...
+
+        }
+
+        private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.HamburgerMenuControl.SetCurrentValue(ContentProperty, e.ClickedItem);
+            this.HamburgerMenuControl.SetCurrentValue(HamburgerMenu.IsPaneOpenProperty, false);
+        }
+    }
+}
---
+++ Views/PrivateView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MahAppsMaterialHamburger.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class PrivateView : UserControl
+    {
+        public PrivateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
